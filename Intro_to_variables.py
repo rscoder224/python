7f44e06260449fc39a4affea92fe51ce4c167008
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-02-01 21:27:08 +0530

    Create Intro_to_variables.py
    In this I learn about variables in python

---
+++ Intro_to_variables.py
@@ -0,0 +1,20 @@
+
+# variables in python 
+
+a = 1  # it an integer type variable
+
+b = 3.33 # it is boolean type variable
+
+c = "kumar" # it is an string type variable 
+
+b = False # it is an boolean type variable 
+
+d = None # it is an none type variable 
+
+b += 3
+print(b);
+
+a -= 2
+
+
+print(a)
