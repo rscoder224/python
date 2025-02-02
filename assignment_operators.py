81afd46b1c4f3bc2afb9fe549f006f6e3d09ba62
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-02-02 12:44:26 +0530

    Create assignment_operators.py

---
+++ assignment_operators.py
@@ -0,0 +1,37 @@
+#  assignment operators
+
+
+x = 20
+
+x += 20
+
+print(x);
+
+
+y = 100
+
+y -= 50;
+
+print(y);
+
+
+z = 2
+
+z *= 6
+
+print(z);
+
+
+a = 12 
+
+a /= 7
+
+print(a);
+
+
+
+
+
+
+
+
