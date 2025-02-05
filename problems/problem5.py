75b5f217a8f53c62256a800b7d69c40700a0b0cb
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-02-05 21:28:16 +0530

    Add files via upload

---
+++ problems/problem5.py
@@ -0,0 +1,56 @@
+
+#  Practice set 
+
+# problem 1
+# write a program to add to numbers in python
+
+
+num_a = int(input("enter a number A"));
+num_b = int(input("enter a number B"));
+
+print("the sum of a and b is", num_a + num_b);
+
+# problem 2
+# write a program to find remainder when a number is divoded by z
+
+
+num1 = 62
+
+num2 = 3
+
+print( "the remainder is " , num1%num2);
+
+
+
+# problem 3
+# check the type of variable assigned using input() funtion
+
+
+check_type = int(input("Enter a Valid Number"))
+
+print(type(check_type), check_type);
+
+
+# problem 4
+# Use comparsion operator to find out weather a given variable a is greater than 'b' or not . Take s = 34 t = 80
+
+
+s = 34
+
+t = 80
+
+print(s > t);
+
+
+# problem 5
+# Write a program to find an average of two numbers entered by the user.
+
+avg_num1 = int(input("enter number 1"))
+avg_num2 = int(input("enter number 1"))
+
+
+print(avg_num1+avg_num2/2);
+
+
+
+
---
+++ problems/problem6.py
@@ -0,0 +1,9 @@
+# problem 6
+# write a python program to calculate the square of a number entered by the user
+
+
+a = int(input("enter number to get square "))
+
+print(a**2);
+
+
