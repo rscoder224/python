42f6049aaf40a3f8f0c67f13bbbc620e0621aa63
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-02-06 21:58:47 +0530

    Create string_functions.py

---
+++ string_functions.py
@@ -0,0 +1,15 @@
+name = "shaurya"
+
+print(len(name));
+
+
+print(name.endswith("rya"))
+
+print(name.startswith("sha"))
+
+
+string1 = " Shaurya is a bad boy"
+
+string2 = string1.replace("bad", "good")
+
+print(string2)
