a831a7158335d791557d0a006652bb33f2d7ff3d
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-02-06 21:57:28 +0530

    Create intro_Strings.py

---
+++ intro_Strings.py
@@ -0,0 +1,33 @@
+# strings
+# string is a datatype in python 
+#  string is a sequence of characters  eclosed in quotes
+
+#  WE can primarly write a string in these three ways 
+#  a = "harry"  Using double quotes
+#  b = 'shaurya' Using Single quotes 
+#  c = ''' Divyanshu ''' Using triple quotes
+
+name = "shaurya"
+
+lastName = "Tiwari"
+
+print(lastName[0:3]);
+
+
+# nameShort = name[0:7]
+# print(nameShort);
+
+
+
+# negative 
+#  print shaurya using negative indexing 
+
+print(name[-7: -1])
+
+
+kall = "meraBhai"
+
+# same as 0:8 
+
+print(kall[-8: -1]); 
+
