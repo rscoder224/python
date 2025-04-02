708e461b851ef3cb91595b7ba3958f5ddb6a2877
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-02 22:27:12 +0530

    Create StringConcatination.py
    string Indexing 
    and String Concatenation

---
+++ StringConcatination.py
@@ -0,0 +1,14 @@
+# string Concatination using + operator in python
+
+name = "Shaurya,"
+
+address = "Adarsh Vihar Colony Buddheswar Lucknow"
+
+print(name[0]);
+
+
+deliverAt = "Deliver at address:--" + "  " + name + " " + address
+
+print(deliverAt);
+
+
