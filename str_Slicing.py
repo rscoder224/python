30f2a35d27fcf31c8560a33ce0e454b6aaeb7332
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-02 22:48:48 +0530

    Create str_Slicing.py
    Slicing,
    String Function

---
+++ str_Slicing.py
@@ -0,0 +1,45 @@
+# slicing in python;
+
+str = "Shaurya Bhai ZindaBaad";
+
+print(str[7:])
+
+# String Function
+
+str2 = "i am coder"
+
+
+print(str2.endswith("der"))  
+# result True
+
+print(str2.capitalize())
+
+str3 = "hello ! I am self Learning Python"
+
+print(str3.replace('Python','JavaScript'))
+
+
+print(str.find('JavaScript'));
+
+print(str.count('e'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
