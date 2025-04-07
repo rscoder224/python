238c592ef1916debbdcc4dd9753bfe15ceb0abe7
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-07 11:08:36 +0530

    Create string1_functions.py

---
+++ string1_functions.py
@@ -0,0 +1,16 @@
+
+str = "Hello bro this Shaurya"
+
+# returns first index of first ouccurer
+print(str.find("Hello")) 
+
+# replace all ouccurerences of bro with fro
+print(str.replace('bro','fro'));
+
+
+# return true value if string substr
+print(str.endswith("rya"))
+
+# count the ouccerence of substring
+print(str.count("am"))
+
